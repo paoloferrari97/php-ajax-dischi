fe373014702ccb15f67016c1a09f6dec4bbf5632
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-24 15:38:21 +0200

    Creo file php con vari link e script per iniziare

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+    <!-- MyCSS -->
+    <link rel="stylesheet" href="./dist/css/app.css">
+</head>
+
+<body>
+
+
+
+    <!-- VueJS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
+        integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- MyScript -->
+    <script src="./dist/js/app.js"></script>
+</body>
+
+</html>
